cf0ded7f5ea8d4c9adf2fe9daa92f40eaa40df19
Tim gong <gonghaokaka@gmail.com>
2021-12-02 23:43:53 +1100

    first two days

---
+++ Day1/index.js
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+
+const data = fs.readFileSync('q1.txt', 'utf8')
+    .toString().split("\r\n").map((line) => +line)
+
+const result = data.reduce((res, _, ix, array) => {
+    return array[ix] > array[ix-1] ? res+1 : res
+}, 0)
+console.log(`AOC-2021-Q1P1:${result}`)
+
+const result_2 = 
+    data.reduce((res, _, ix, array) => {
+        return array[ix+2] ? [...res, array[ix] + array[ix+1] + array[ix+2]] : res
+    }, [])
+    .reduce((res, _, ix, array) => {
+        return array[ix] > array[ix-1] ? res+1 : res
+    }, 0)
+console.log(`AOC-2021-Q1P2:${result_2}`)
---
+++ Day2/index.js
@@ -0,0 +1,42 @@
+import * as fs from 'fs';
+
+const data = fs.readFileSync("q2.txt", "utf8")
+    .toString().split("\r\n")
+    .map((line) => {
+        let comm = line.split(" ")[0]
+        let value = +line.split(" ")[1]
+        return {"comm": comm, "value": value}
+    })
+
+const cord = data.reduce((res, instrunction) => {
+    switch (instrunction.comm){
+        case 'forward':
+            res.x += instrunction.value
+            break
+        case 'down':
+            res.y += instrunction.value
+            break
+        case 'up':
+            res.y -= instrunction.value
+            break
+    }
+    return res
+}, {x:0, y:0})
+console.log(`AOC-2021-Q2P1:${cord.x * cord.y}`)
+
+const cord_2 = data.reduce((res, instrunction) => {
+    switch (instrunction.comm){
+        case 'forward':
+            res.x += instrunction.value
+            res.y += res.aim * instrunction.value
+            break
+        case 'down':
+            res.aim += instrunction.value
+            break
+        case 'up':
+            res.aim -= instrunction.value
+            break
+    }
+    return res
+}, {aim:0, x:0, y:0})
+console.log(`AOC-2021-Q2P2:${cord_2.x * cord_2.y}`)
